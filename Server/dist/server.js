e041c20c9b6002f858403838d1fb682de025d4d3
Mahee059 <munakhatiwada2059@gmail.com>
2026-01-13 16:48:14 +0545

    first commit

---
+++ Server/dist/server.js
@@ -0,0 +1,19 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+require("dotenv/config");
+//initialize express
+const app = (0, express_1.default)();
+//Middlewares
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
+//routes
+const PORT = process.env.PORT 
+app.listen(PORT, () => {
+    console.log(`Server running on PORT: ${PORT}`);
+});
